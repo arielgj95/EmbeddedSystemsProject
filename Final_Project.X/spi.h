e25b7008af5688ed2e90ed73d4f14f6e046f1f33
Franc95 <48449544+Franc95@users.noreply.github.com>
2020-10-09 17:36:18 +0200

    Update spi.h

--- Final_Project.X/spi.h
+++ Final_Project.X/spi.h
@@ -13,7 +13,6 @@
 
 void init_spi();
 void clear_row_LCD(int flag);
-void shift_string(char* str , int n, int shiftsize);
 void write_string_LCD(char* str, int max);
 void write_long_string_LCD(char* str, int counter);
 void move_cursor_first_row();
